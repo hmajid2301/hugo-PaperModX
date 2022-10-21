71f2210d058454b518ec65987165079a6b22c75a
Haseeb Majid <haseeb@joinzoe.com>
2022-10-21 14:22:38 +0100

    work

--- assets/js/fastsearch.js
+++ assets/js/fastsearch.js
@@ -20,7 +20,6 @@ showButtonMobile? showButtonMobile.addEventListener("click", displaySearch) : nu
 hideButton.addEventListener("click", hideSearch);
 wrapper.addEventListener("click", hideSearch);
 modal.addEventListener("click", function (event) {
-  console.log("HELLO")
   event.stopPropagation();
   event.stopImmediatePropagation();
   return false;
@@ -140,14 +139,12 @@ function executeQuery(term) {
       resultsHTML =
         resultsHTML +
         `<li class="mb-2">
-          <a class="flex items-center px-3 py-2 rounded-md appearance-none bg-neutral-100 dark:bg-neutral-700 focus:bg-primary-100 hover:bg-primary-100 dark:hover:bg-primary-900 dark:focus:bg-primary-900 focus:outline-dotted focus:outline-transparent focus:outline-2" href="${value.item.permalink}" tabindex="0">
+          <a class="search-item" href="${value.item.permalink}" tabindex="0">
             <div class="grow">
               <div class="-mb-1 text-lg font-bold">${value.item.title}</div>
-              <div class="text-sm text-neutral-500 dark:text-neutral-400">${value.item.section}<span class="px-2 text-primary-500">&middot;</span>${value.item.date}</span></div>
+              <div class="text-sm text-neutral-500 dark:text-neutral-400">${value.item.section}</div>
               <div class="text-sm italic">${value.item.summary}</div>
             </div>
-            <div class="ml-2 ltr:block rtl:hidden text-neutral-500">&rarr;</div>
-            <div class="mr-2 ltr:hidden rtl:block text-neutral-500">&larr;</div>
           </a>
         </li>`;
     });
